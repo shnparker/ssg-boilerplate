2cd33e8f96b771a840e4fda71eb0c89b0038083e
Shane Parker <shane@bazaruto.com>
2020-06-18 20:24:20 +0200

    fix: tailwind purge locations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  purge: ["./components/**/*.js", "./pages/**/*.js"],
+  purge: ['.src/components/**/*.jsx', './pages/**/*.jsx'],
   theme: {
     extend: {},
   },
   variants: {},
-  plugins: [require("@tailwindcss/ui")],
+  plugins: [require('@tailwindcss/ui')],
 };
