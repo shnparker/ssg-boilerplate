b8577415f3c19bd81a717b46ca3d37b6b5d0b74f
Shane Parker <shane@bazaruto.com>
2020-06-29 22:04:44 +0200

    feat: custom document

---
+++ src/pages/_document.jsx
@@ -0,0 +1,47 @@
+import * as Sentry from '@sentry/node';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import React from 'react';
+
+/**
+ * Send to Sentry all unhandled rejections & exceptions.
+ * @see https://leerob.io/blog/configuring-sentry-for-nextjs-apps
+ */
+process.on('unhandledRejection', (err) => {
+  Sentry.captureException(err);
+});
+
+process.on('uncaughtException', (err) => {
+  Sentry.captureException(err);
+});
+
+/**
+ * _document.jsx
+ * See https://github.com/vercel/next.js/#custom-document
+ */
+class AppDocument extends Document {
+  static async getInitialProps(ctx) {
+    Sentry.addBreadcrumb({
+      category: 'pages/_document',
+      message: `Rendering _document`,
+      level: Sentry.Severity.Debug,
+    });
+  
+    const initialProps = await Document.getInitialProps(ctx);
+  
+    return { ...initialProps };
+  }
+  
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body className={`stage-${process.env.NEXT_PUBLIC_APP_STAGE}`}>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+  
+export default AppDocument;
