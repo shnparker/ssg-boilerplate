1b57560e64c7660802b1b2832edbbd92700c1c25
Shane Parker <shane@bazaruto.com>
2020-07-09 10:41:12 +0200

    enh:: defensive context code

--- src/hooks/useUserSession.jsx
+++ src/hooks/useUserSession.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import userSessionContext from "../stores/userSessionContext";
 
-/**
- * Hook to access the user session data
- *
- * Uses userSessionContext internally (provides an identical API)
- *
- * This hook should be used by components in favor of userSessionContext directly,
- * because it grants higher flexibility if you ever need to change the implementation (e.g: use something else than React.Context, like Redux/MobX)
- *
+/*
  * @see https://slides.com/djanoskova/react-context-api-create-a-reusable-snackbar#/11
  */
 const useUserSession = () => {
-  return React.useContext(userSessionContext);
+  const context = React.useContext(userSessionContext);
+
+  if (typeof context === "undefined") {
+    throw new Error("useUserSession must be used within UserSessionProvider");
+  }
+
+  return context;
 };
 
 export default useUserSession;
